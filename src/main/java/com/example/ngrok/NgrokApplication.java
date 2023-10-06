0c74b143632ff3b477f61c219c6625b4e4ab1676
Sergii Kozhushko <sergiikozhushko@gmail.com>
2023-10-06 11:46:54 +0200

    Added modules client and server

---
+++ src/main/java/com/example/ngrok/NgrokApplication.java
@@ -0,0 +1,13 @@
+package com.example.ngrok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgrokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NgrokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ngrok/NgrokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ngrok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgrokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
