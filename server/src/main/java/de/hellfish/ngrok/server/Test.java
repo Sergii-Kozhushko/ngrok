15181d86a3a03029fe7604cf0ff426d10b9d2fb4
Sergii Kozhushko <sergiikozhushko@gmail.com>
2023-10-08 21:26:34 +0200

    add github actions, checkstyle

---
+++ server/src/main/java/de/hellfish/ngrok/server/Test.java
@@ -0,0 +1,17 @@
+package de.hellfish.ngrok.server;
+
+/**
+ *
+ */
+public class Test {
+    private int val1;
+
+    public static void main(String[] args) {
+        someFunc();
+    }
+
+    private static void someFunc() {
+        int i = 0;
+
+    }
+}
