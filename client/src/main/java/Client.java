34511dcd2b447b4d04c94d902e2f20b6ddacfd25
Sergii Kozhushko <sergiikozhushko@gmail.com>
2023-10-17 19:33:16 +0200

    structure, client

---
+++ client/src/main/java/Client.java
@@ -0,0 +1,107 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+    private static final int clientPort = 8082;
+    private static int servicePort = 2222;
+    private static final String serverHost = "localhost";
+    private static final int serverPort = 8082;
+    private static boolean running = true;
+
+    public static void main(String[] args) {
+        // аргумент строки запуска -service-port=номер_порта сервиса
+        for (String arg : args) {
+            if (arg.startsWith("-service-port")) {
+                servicePort = Integer.parseInt(arg.substring("-service-port=".length()));
+            }
+        }
+
+        try  {
+            Socket socket = new Socket(serverHost, serverPort);
+            PrintWriter serverOut = new PrintWriter(socket.getOutputStream());
+            BufferedReader serverIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            serverOut.print("HTTP " + servicePort +"\n");
+            serverOut.flush();
+            String line;
+            StringBuilder request = new StringBuilder();
+            while (!serverIn.ready()) ;
+            String serverReply = serverIn.readLine();
+
+            // сервер прислал ошибку
+            if (serverReply.split(" ")[0].equals("ERROR")) {
+                System.out.println(serverReply.substring("ERROR ".length()));
+                running = false;
+            } else {
+                String link = serverReply.split(" ")[1];
+                System.out.println("Link for user requests: http://localhost:" + servicePort + " -> " + link);
+            }
+
+            while (running) {
+                while (!serverIn.ready()) ;
+                while ((line = serverIn.readLine()) != null && !line.isEmpty()) {
+                    request.append(line).append("\r\n");
+                }
+                System.out.println("Client got message from server: \n" + request);
+                // получить номер юзера
+                String userNumber = request.toString().split("\r\n")[0].split(" ")[1];
+                String message = request.substring(("USER " + userNumber+"\r\n").length());
+
+                // переслать полученное сообщение сервису и получить ответ
+                StringBuilder responseFromService = exchangePacketsWithService(message);
+
+                System.out.println("Client got response from service:\n" + responseFromService);
+
+                // переслать ответ сервиса серверу
+                String response = "USER " + userNumber + "\r\n" + responseFromService + "\n";
+                serverOut.print(response);
+                serverOut.flush();
+
+            }
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("Server is not reachable on " + serverHost + ":" + serverPort);
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Method sends message to service and get reply
+     * @param request
+     * @return
+     */
+    private static StringBuilder exchangePacketsWithService(String request) {
+        StringBuilder response = new StringBuilder();
+        try
+        {
+            Socket socket = new Socket("localhost", servicePort);
+            // поток для отправки запроса на сервис
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            //отправить запрос сервису
+            out.print(request + "\n");
+            out.flush();
+
+            // принимаем ответ
+            String line;
+            while ((line = in.readLine()) != null ) {
+                response.append(line).append("\r\n");
+            }
+
+        } catch (
+                IOException ex) {
+            ex.printStackTrace();
+        }
+        return response;
+    }
+
+}
---
+++ client/src/main/java/TestService.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class TestService {
+    private static final int testServicePort = 2222;
+    private static final int clientPort = 8082;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(testServicePort)) {
+            System.out.println("Test Service started! Listening on: " + testServicePort);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                // пришел запрос на тунель
+                System.out.print("Someone connected. ");
+                System.out.println(" Host: " + socket.getLocalAddress() + ":" + socket.getPort());
+
+                try (
+                        // поток чтения запроса от клиента
+                        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(socket.getInputStream(),
+                                        StandardCharsets.UTF_8));
+                        //  поток ответа
+                        PrintWriter writer = new PrintWriter(socket.getOutputStream())
+                ) {
+
+                    // ждем входящие данные
+                    while (!reader.ready()) ;
+                    // читаем запрос
+                    StringBuilder request = new StringBuilder();
+                    while (reader.ready()) {
+                        request.append(reader.readLine());
+                        request.append("\r\n");
+
+                    }
+                    System.out.println("TestService got request:\n" + request);
+
+                    String reply200 = "HTTP/1.1 200 OK\r\n" +
+                            "Date: Fri, 14 Oct 2023 10:00:00 GMT\r\n" +
+                            "Server: Apache/2.4.41 (Unix)\r\n" +
+                            "Content-Length: 4\r\n" +
+                            "Content-Type: text/plain; charset=utf-8" +
+                            "\r\n\r\n" +
+                            "test";
+
+                    writer.write(reply200);
+
+
+                    // отправить ответ клиенту
+//                    writer.print("HTTP/1.1 418\n");
+//                    writer.print("Content-Type: text/html; charset=utf-8\n");
+//                    writer.write("Content-Length: 0\n\n");
+
+
+                    writer.flush();
+                    // здесь надо соедениться с клиентом ngrok и передать ему запрос
+
+                }
+            }
+
+        } catch (
+                IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+
+}
